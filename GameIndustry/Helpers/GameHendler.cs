9d2070ef4d50f233e4ad2729199b8f1cefd0ce1a
Daniil211 <id20214@gmail.com>
2023-03-26 23:45:23 +0300

    Timer and IDisposable

---
+++ GameIndustry/Helpers/GameHendler.cs
@@ -0,0 +1,24 @@
+﻿namespace GameIndustry.Helpers
+{
+    public class GameHendler
+    {
+
+        public RSPOptions Choose { get; set; }
+
+        public RSPOptions WinCondition { get; set; }
+
+        public RSPOptions LosseCondition { get; set; }
+
+        public string Image { get; set; }
+
+        public GameState GameResult(GameHendler opponent)
+        {
+            if (Choose == opponent.Choose)
+                return GameState.Draw;
+            if(LosseCondition == opponent.Choose)
+                return GameState.Loss;
+            return GameState.Victory;
+        }
+    }
+  
+}
