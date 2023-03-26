ffbc2fec552e1de02931573921007ac5c0d49e4a
Daniil211 <id20214@gmail.com>
2023-03-27 01:33:46 +0300

    routing(layout of a new project)

---
+++ GameIndustry_v2/GameIndustry_v2/Models/GameModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.AccessControl;
+
+namespace GameIndustry_v2.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Image { get; set; }
+    }
+}
