603cecc538a8299d85578e5805dae6ab9f84182d
Daniil211 <id20214@gmail.com>
2023-03-26 20:21:15 +0300

    CascadingParameter

---
+++ GameIndustry/Models/AppStyle.cs
@@ -0,0 +1,23 @@
+﻿namespace GameIndustry.Models
+{
+    public class AppStyle
+    {
+        public string Color { get; set; } = "green";
+
+        public string Size { get; set; } = "65px";
+
+        public List<string> WitcherWords { get; set; } = new List<string>()
+        {
+            new string("AAAAAAAAA AAAAAAA AAAAAAAAAA AAAAAAAAAAAAA AAAAAAAAAA AAAAAAA(Witcher)"),
+            new string("BBBBB BBBB BBBBB    BBB BB B(Witcher)"),
+            new string("CCCCCC CCCCCCCC C CCCC CCCCCCCCCCC CCCCCCCC(Witcher)")
+        };
+
+        public List<TableListModel> TableStyle { get; set; } = new List<TableListModel>()
+        {
+            new TableListModel(){Text = "Table Dark", Value="table-dark"},
+            new TableListModel(){Text = "Table Striped", Value="table-striped"},
+            new TableListModel(){Text = "Table Bordered", Value="table-bordered"}
+        };
+    }
+}
--- GameIndustry/Pages/Counter.razor.cs
+++ GameIndustry/Pages/Counter.razor.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using GameIndustry.Models;
+using Microsoft.AspNetCore.Components;
+using static GameIndustry.Shared.MainLayout;
 
 namespace GameIndustry.Pages
 {
     public partial class Counter
     {
 
-        [Inject] SingletonService singleton { get; set; }
+        [Inject] 
+        SingletonService singleton { get; set; }
 
-        [Inject] TrinsientService transient { get; set; }
+        [Inject] 
+        TrinsientService transient { get; set; }
+
+        //[CascadingParameter(Name = "Color")]
+        //public string Color { get; set; }
+
+        //[CascadingParameter(Name = "Size")]
+        //public string Size { get; set; }
+
+        [CascadingParameter] public AppStyle Styles { get; set; }
 
         private int currentCount = 0;
 
