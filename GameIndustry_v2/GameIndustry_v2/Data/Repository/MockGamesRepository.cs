e4b61ec809021e2c2da5c1213bfe02218e213da9
Daniil211 <id20214@gmail.com>
2023-03-27 16:10:42 +0300

    Form HTML

--- GameIndustry_v2/GameIndustry_v2/Data/Repository/MockGamesRepository.cs
+++ GameIndustry_v2/GameIndustry_v2/Data/Repository/MockGamesRepository.cs
@@ -16,21 +16,21 @@ namespace GameIndustry.Repository
             new GameModel()
             {
                 Name = "Farming Simulator 2019",
-                Genre = "Simulator",
+                GenreId = 4,
                 ReleaseDate = new DateTime(2018, 11, 19),
                 Image = "https://cdn1.epicgames.com/epic/offer/Diesel_productv2_farming-simulator-19_home_EGS-GIANTS-FS19-860x1148_Standard-860x1148-a464360aa738f506399aff4c95d0a9c7021dee5f-860x1148-71df4a510f82c42cebe1b1861d2f77a3.jpg"
             },
             new GameModel()
             {
                 Name = "The Witcher 3",
-                Genre = "Action/RPG",
+                GenreId = 0,
                 ReleaseDate = new DateTime(2015, 05, 18),
                 Image = "https://upload.wikimedia.org/wikipedia/ru/a/a2/The_Witcher_3-_Wild_Hunt_Cover.jpg"
             },
             new GameModel()
             {
                 Name = "Destroy All Humans!",
-                Genre = "Action-Adventure",
+                GenreId = 1,
                 ReleaseDate = new DateTime(2020, 07, 28),
                 Image = "https://cdn1.epicgames.com/c12a5687fb024192999b2d22c555d4c6/offer/DAH_Portrait-1200x1600-2e2b6bca291228e4df252c4857664408.jpg"
             }
@@ -55,7 +55,12 @@ namespace GameIndustry.Repository
                 new Genre()
                 {
                     Id=3,
-                    Name="Paltformer"
+                    Name="Platformer"
+                },
+                new Genre()
+                {
+                    Id=4,
+                    Name="Simulator"
                 }
             };
         }
--- GameIndustry_v2/GameIndustry_v2/Models/GameModel.cs
+++ GameIndustry_v2/GameIndustry_v2/Models/GameModel.cs
@@ -8,10 +8,23 @@ namespace GameIndustry_v2.Models
 
         public string Name { get; set; }
 
-        public string Genre { get; set; }
+        public int GenreId { get; set; }
 
         public DateTime ReleaseDate { get; set; }
 
         public string Image { get; set; }
+
+        public string ShortName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Name))
+                    return null;
+                if(Name.Length > 60)
+                    return Name.Substring(0, 57) + "...";
+                else
+                    return Name;
+            }
+        }
     }
 }
